271fa6d75aa674afd64f26288837e0825aec644d
decentboysanthosh <jaggampudisanthosh7@gmail.com>
2025-10-30 11:08:39 +0530

    Add files via upload

---
+++ greater of 3 numbers.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the values of a: ");
+	scanf("%d",&a);
+	printf("enter the values of b: ");
+	scanf("%d",&b);
+	printf("enter the values of c: ");
+	scanf("%d",&c);
+	if
+	(a>b&&a>c){
+	printf("a is larger number");
+	}
+	else
+	if(b>c&&b>a)
+	printf("b is larger number");
+	else
+	printf("c is larger number");
+	
+	
+}
+
