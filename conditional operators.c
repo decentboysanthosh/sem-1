11ebfabe70ba099db849f2ff33ac164d1ab91ae2
JAGGAMPUDI SANTHOSH <jaggampudisanthosh7@gmail.com>
2025-10-30 14:05:28 +0530

    Add files via upload

---
+++ conditional operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,max;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	printf("Enter the value of c:");
+	scanf("%d",&c);
+	max=a>b?(a>c?a:c):(b>c?b:c);
+	printf("Greater of a,b,c is %d",max);
+}
