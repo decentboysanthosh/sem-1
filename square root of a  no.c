8da94c011af77c4a9181b1f1565e5605758fc0b8
decentboysanthosh <jaggampudisanthosh7@gmail.com>
2025-10-30 13:39:37 +0530

    Add files via upload

---
+++ square root of a  no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int num;
+	float res;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	res=sqrt(num);
+	printf("%f",res);
+}
---
+++ sum of N natural numbers.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	i=n*((n+1)/2);
+	printf("Enter the value of n:");
+	scanf("%d ",&n);
+	for(i=1;i<=n;i++)
+	printf("sum of given nos is %d",i);
+	}
