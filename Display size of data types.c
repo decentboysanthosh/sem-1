a88e8a8bfb0bf47a593ddb3a0bbb9f0db8b31781
JAGGAMPUDI SANTHOSH <jaggampudisanthosh7@gmail.com>
2025-10-30 14:00:16 +0530

    Add files via upload

---
+++ Display size of data types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+printf("Size of int:1%u bytes \n");
+sizeof(int);
+printf("Size of float:%1u bytes \n");
+sizeof(float);
+printf("Size of char:%1u bytes \n");
+sizeof(char);
+printf("Size of double:%1u bytes \n");
+sizeof(double);
+printf("Size of long:%1u bytes \n");
+sizeof (long);
+return 0;
+}
